e3e97d62e56932fb62e7983ff16bcba163349d28
keproost <kevin.proost@dbits.eu>
2019-10-09 16:46:08 +0200

    First commit - First theme tweaks - adjusted gulp with extra monitor for scss - adapted core variable files - added styling in style.scss - Basic page setup for rdm

---
+++ app-assets/js/scripts/configs/horizontal-menu.js
@@ -0,0 +1,16 @@
+// Menu Icon Color Config
+var menuIconColorsObj = {
+  iconStrokeColor    : "#8494A7",
+  iconSolidColor     : "#8494A7",
+  iconFillColor      : "#8494A7",
+  iconStrokeColorAlt : "#8494A7"
+};
+
+
+// Active Menu Icon Color Config
+var menuActiveIconColorsObj = {
+  iconStrokeColor    : "#ED6D05",
+  iconSolidColor     : "#ED6D05",
+  iconFillColor      : "#ED6D05",
+  iconStrokeColorAlt : "#ED6D05"
+};
